85ff4bacfa567a44e18d4423a89be237819cfbbd
Ravi Cheetirala <xrca4@woolworths.com.au>
2019-06-17 09:49:08 +1000

    Updated

--- BookingLookupService/src/main/java/com/wow/erc/booking/BookingLookupServiceApplication.java
+++ BookingLookupService/src/main/java/com/wow/erc/booking/BookingLookupServiceApplication.java
@@ -1,7 +1,5 @@
 package com.wow.erc.booking;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,11 +12,13 @@ public class BookingLookupServiceApplication {
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
-        String[] beanNames = ctx.getBeanDefinitionNames();
+      /*  String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
-        }
+        }*/
     }
+    
+    
 
 }
--- BookingLookupService/src/main/java/com/wow/erc/booking/controller/BookingController.java
+++ BookingLookupService/src/main/java/com/wow/erc/booking/controller/BookingController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,16 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 
+@CrossOrigin
 @RequestMapping("/Booking")
 @Api(value = "BookingController", description = "Used to manage Booking Lookup operations")
 @RestController
+
 public class BookingController {
 	
 	@Autowired
 	private BookingService bookingService;
-	
+	@CrossOrigin
 	@RequestMapping("/getBookingInfo")
 	@ApiOperation(value = "Booking information will be fetched with this service", response = Iterable.class)
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved list"),
